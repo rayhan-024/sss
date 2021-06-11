8a6c55e256d28a03f2e3f6c9a7b8735dbc54d262
Rayhan <rayhan1615024@gmail.com>
2021-06-12 03:45:55 +0600

    allowed

--- testproject2/settings.py
+++ testproject2/settings.py
@@ -18,7 +18,8 @@ SECRET_KEY = 'django-insecure-*!(mzv%=h@ne3zu#w5drve+ys@ll5-67m&2vqe($%f1cp-@p89
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
+['localhost', '127.0.0.1', ]
 
 
 # Application definition
